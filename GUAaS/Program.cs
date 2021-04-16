46ab4d8187523e725dd202da2dea34a4eb43b3c5
Oleg <serv_all@mail.ru>
2021-04-16 18:15:09 +0300

    Реализован алгоритм согласно блок-схемы задания 1

--- GUAaS/Program.cs
+++ GUAaS/Program.cs
@@ -4,9 +4,47 @@ namespace GUAaS
 {
     class Program
     {
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n;
+            do
+            {
+                Console.Write("input number: ");
+            } while (!int.TryParse(Console.ReadLine(), out n));
+            Console.WriteLine($"number is simple: {IsNumberIsSimple(n)}");
+
+
+
+
+        }
+
+        private static bool IsNumberIsSimple(int n)
+        {
+            int d = 0,
+                i = 2;
+
+            while (i < n) //сложность O(n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+
+            if (d == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
