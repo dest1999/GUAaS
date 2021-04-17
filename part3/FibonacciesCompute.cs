4265d94dba78b38ef04e66bd16714cc18de9d0ed
Oleg <serv_all@mail.ru>
2021-04-17 22:45:05 +0300

    Выполнены тесты к части 3 (ряды Фибоначчи)

--- part3/FibonacciesCompute.cs
+++ part3/FibonacciesCompute.cs
@@ -6,7 +6,8 @@ namespace part3
     {
         static void Main(string[] args)
         {
-            const int n = 46;//вычисляем от 46
+            
+            const int n = 2;//вычисляем от 46
 
             //далее сравнение производительности цикла и рекурсии
             DateTime timeRec = DateTime.Now;
@@ -14,15 +15,20 @@ namespace part3
             TimeSpan tsRec = DateTime.Now - timeRec;
 
             DateTime timeCicle = DateTime.Now;
-            int resultC = FibonacciCicle(n);
+            int resultC = FibonacciCycle(n);
             TimeSpan tsCicle = DateTime.Now - timeCicle;
 
             Console.WriteLine($"Recursively: {resultR}, time: {tsRec}");
             Console.WriteLine($"Cicle: {resultC}, time {tsCicle}");
         }
 
-        public static int FibonacciCicle(int n) //сложность метода O(n)
+        public static int FibonacciCycle(int n) //сложность метода O(n)
         {
+            if (n < 0)
+            {
+                throw new Exception("Argument can not be negative");
+            }
+
             int a = 0, b = 1, tmp;
 
             for (int i = 0; i < n; i++)
@@ -38,6 +44,11 @@ namespace part3
 
         public static int FibonacciRecurse(int n) //сложность метода O(n^2)
         {
+            if (n < 0)
+            {
+                throw new Exception("Argument can not be negative");
+            }
+
             if (n == 0 || n == 1)
             {
                 return n;
---
+++ part3Tests/FibonacciesComputeTests.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using part3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part3.Tests
+{
+    [TestClass()]
+    public class FibonacciesComputeTests
+    {
+        //данные для тестирования
+        readonly int[,] testArray = { { 0, 2, 5, 9, 43, 46 }, { 0, 1, 5, 34, 433494437, 1836311903 } };
+        int index, actual;
+
+        //тестирование цикла
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void FibonacciCycleTest_InputNegative_ReturnExeption()
+        {
+            FibonacciesCompute.FibonacciCycle(-5);
+        }
+
+
+        [TestMethod()]
+        public void FibonacciCycleTest_testArray_Element0()
+        {
+            index = 0;
+            actual = FibonacciesCompute.FibonacciCycle(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciCycleTest_testArray_Element1()
+        {
+            index = 1;
+            actual = FibonacciesCompute.FibonacciCycle(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciCycleTest_testArray_Element2()
+        {
+            index = 2;
+            actual = FibonacciesCompute.FibonacciCycle(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciCycleTest_testArray_Element3()
+        {
+            index = 3;
+            actual = FibonacciesCompute.FibonacciCycle(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciCycleTest_testArray_Element4()
+        {
+            index = 4;
+            actual = FibonacciesCompute.FibonacciCycle(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciCycleTest_testArray_Element5()
+        {
+            index = 5;
+            actual = FibonacciesCompute.FibonacciCycle(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        //далее тестирование рекурсии
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void FibonacciRecurseTest_InputNegative_ReturnExeption()
+        {
+            FibonacciesCompute.FibonacciRecurse(-5);
+
+        }
+
+        [TestMethod()]
+        public void FibonacciRecurseTest_testArray_Element0()
+        {
+            index = 0;
+            actual = FibonacciesCompute.FibonacciRecurse(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciRecurseTest_testArray_Element1()
+        {
+            index = 1;
+            actual = FibonacciesCompute.FibonacciRecurse(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciRecurseTest_testArray_Element2()
+        {
+            index = 2;
+            actual = FibonacciesCompute.FibonacciRecurse(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciRecurseTest_testArray_Element3()
+        {
+            index = 3;
+            actual = FibonacciesCompute.FibonacciRecurse(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciRecurseTest_testArray_Element4()
+        {
+            index = 4;
+            actual = FibonacciesCompute.FibonacciRecurse(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+        [TestMethod()]
+        public void FibonacciRecurseTest_testArray_Element5()
+        {
+            index = 5;
+            actual = FibonacciesCompute.FibonacciRecurse(testArray[0, index]);
+
+            Assert.AreEqual(testArray[1, index], actual);
+        }
+
+    }
+}
