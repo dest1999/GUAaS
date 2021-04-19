b17600f48a1ef9b74708c1a1b64a50b5f8c773af
Oleg <serv_all@mail.ru>
2021-04-19 22:17:51 +0300

    ДЗ-2

--- GUAaS/Program.cs
+++ GUAaS/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+//TODO Требуется реализовать класс двусвязного списка и операции вставки, удаления и поиска элемента в нём в соответствии с интерфейсом
 namespace GUAaS
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
